59f9e12c50c6e97af9e1b69fb335c5bc587fc7bc
Solkar <87309820+S0lkar@users.noreply.github.com>
2023-10-27 14:44:48 +0200

    [Clon web] Fase I completada
    > Base terminada, con una sidebar con el gradiente de repsol (la usaré de forma provisional para el chatbot hasta que de con la tecla de como hacerlo más personalizado, si lo vemos necesario)
    
    - Intenté arreglar el txt corrupto....pero no hay forma

--- Tests/Clon.py
+++ Tests/Clon.py
@@ -1,12 +1,12 @@
 import base64
 import streamlit as st
 import plotly.express as px
-
+# https://codepen.io/lewismcarey/pen/GJZVoG
 df = px.data.iris()
 fondo = "background.png"
 
 
-@st.experimental_memo
+@st.cache_data #@st.experimental_memo
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
@@ -18,18 +18,16 @@ img = get_img_as_base64(fondo)
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("background.png");
-background-size: 180%;
+background-image: url("data:image/png;base64,{img}");
+background-size: 100%;
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
 
 [data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
+background: rgb(168,23,40);
+background: linear-gradient(0deg, rgba(168,23,40,0.8) 0%, rgba(214,29,49,0.8) 8%, rgba(250,250,250,0.8) 44%, rgba(238,128,10,0.8) 82%, rgba(250,189,60,0.8) 100%);
 }}
 
 [data-testid="stHeader"] {{
@@ -43,30 +41,5 @@ right: 2rem;
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.title("Repsol")
-st.sidebar.header("Testeo")
-
-with st.container():
-    st.header("Buenos dias")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-with st.container():
-    st.header("Buenos dias 2")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-with st.container():
-    st.header("Buenas 3")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
-with st.container():
-    st.header("hola 4")
-    st.markdown(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+st.title("")
+st.sidebar.header("")
---
+++ Tests/Dumpster_Clon.py
@@ -0,0 +1,26 @@
+'''
+with st.container():
+    st.header("Buenos dias")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+with st.container():
+    st.header("Buenos dias 2")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+with st.container():
+    st.header("Buenas 3")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+with st.container():
+    st.header("hola 4")
+    st.markdown(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+'''
